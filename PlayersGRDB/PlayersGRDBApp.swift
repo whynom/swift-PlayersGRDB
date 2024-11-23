9f394235738386a81ed36256b8552d09ab7d078e
ynom <nomatter@domain.com>
2024-11-23 11:08:52 -0800

    Initial commit

---
+++ PlayersGRDB/PlayersGRDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayersGRDBApp.swift
+//  PlayersGRDB
+//
+//  Created by ynom on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayersGRDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlayersGRDBTests/PlayersGRDBTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlayersGRDBTests.swift
+//  PlayersGRDBTests
+//
+//  Created by ynom on 11/23/24.
+//
+
+import Testing
+
+struct PlayersGRDBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
